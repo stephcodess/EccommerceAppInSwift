b46c40abca2ff74cbf64589f67b067e244be2aa7
stephcodess <stephcodess>
2023-05-27 08:36:02 +0100

    Initial Commit

---
+++ ecommerce app in swift/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ecommerce app in swift
+//
+//  Created by MacBook Pro on 27/05/R5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ecommerce app in swift/ecommerce_app_in_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecommerce_app_in_swiftApp.swift
+//  ecommerce app in swift
+//
+//  Created by MacBook Pro on 27/05/R5.
+//
+
+import SwiftUI
+
+@main
+struct ecommerce_app_in_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
